70ac59bcba686ca4315a31f51871e0744d03862f
MrClottom <philippe-dumonet@protonmail.com>
2020-10-21 19:48:00 +0200

    Remove complex assembler

--- assemble.py
+++ assemble.py
@@ -1,18 +1,32 @@
 #!/usr/bin/python3
-
-# ECA stands for Ethereum Code Assembly
-
 import sys
-from compile import compile_eca
+import json
+
+
+def load_opcodes():
+    with open('opcodes.json', 'r') as f:
+        op_codes = json.load(f)
+    return op_codes
+
+
+def sanitize(inp):
+    return ' '.join(
+        inp.replace('\n', ' ').replace('0x', '').split()
+    )
+
+
+def compile_main(code):
+    OP_CODES = load_opcodes()
+    return ''.join([OP_CODES.get(block, block)
+                    for block in sanitize(code).split()])
 
 
 def main():
     with open(sys.argv[1], 'r') as f:
-        code = f.read()
+        data = f.read()
 
-    res = compile_eca(code)
-
-    print('res:', res)
+    data = compile_main(data)
+    data = print('data: 0x', data.lower(), sep='')
 
 
 if __name__ == '__main__':
